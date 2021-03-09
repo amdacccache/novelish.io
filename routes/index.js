0c0d17b1a80f29800a99de56e26fbf3c343b24ec
Nabil Arbouz <nabilarbouz33@gmail.com>
2021-03-08 16:47:56 -0800

    fixed default express rendering of index

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var mongodbKey = require("../mongoDBFolder/mongoDBInfo");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  res.sendFile(path.join(__dirname, "/../public/index.html"));
 });
 
 module.exports = router;
