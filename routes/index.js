ac40535a6a6d884bc89547b6090c960fd0fe0cdc
Haoyang Ding <had035@eng.ucsd.edu>
2021-03-17 15:02:15 -0700

    Haoyang Ding's codeReview

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,11 @@
+/*
+* Haoyang Ding => Nabil:
+*
+* I don't recommend you use the sendFile() to do the redirect. That's slow and sometimes cause address error.
+* Try res.redirect() next time.
+*
+* */
+
 var express = require("express");
 var router = express.Router();
 const path = require("path");
